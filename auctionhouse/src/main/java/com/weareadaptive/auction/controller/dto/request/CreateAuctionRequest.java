b5fe19e02953fbb99cfa890195f74a95a5602373
will.wang <superwill.career@gmail.com>
2022-03-09 10:40:35 -0500

    validation constraints

--- auctionhouse/src/main/java/com/weareadaptive/auction/controller/dto/request/CreateAuctionRequest.java
+++ auctionhouse/src/main/java/com/weareadaptive/auction/controller/dto/request/CreateAuctionRequest.java
@@ -1,8 +1,17 @@
 package com.weareadaptive.auction.controller.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public record CreateAuctionRequest(
+    @NotBlank
+    @Size(max = 10)
     String symbol,
+
+    @Positive
     double minPrice,
-    int quantity
-) {
+
+    @Positive
+    int quantity) {
 }
--- auctionhouse/src/main/java/com/weareadaptive/auction/controller/dto/request/CreateBidRequest.java
+++ auctionhouse/src/main/java/com/weareadaptive/auction/controller/dto/request/CreateBidRequest.java
@@ -1,8 +1,19 @@
 package com.weareadaptive.auction.controller.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public record CreateBidRequest(
+
+    @Size(max = 30)
     String owner,
+
+    @Max(300)
+    @Positive
     int quantity,
+
+    @Positive
     double price
 ) {
 }
